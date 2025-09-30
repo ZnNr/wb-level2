c2801021d3c268b8b6b2315856dd11379a17f8a8
Anthony <anthonyo@student.21-school.ru>
2025-09-30 22:07:04 +0300

    task 2-3

--- 2-3/2-3.go
+++ 2-3/2-3.go
@@ -1,5 +1,28 @@
 package main
 
 /*
+Что выведет программа?
 
- */
+Объяснить внутреннее устройство интерфейсов и их отличие от пустых интерфейсов.
+*/
+import (
+	"fmt"
+	"os"
+)
+
+func Foo() error {
+	var err *os.PathError = nil //cоздается переменная err типа *os.PathError
+	//err — это nil-указатель конкретного типа (*os.PathError).
+	return err
+}
+
+func main() {
+	err := Foo()
+	fmt.Println(err)        //<nil>
+	fmt.Println(err == nil) // false
+}
+
+/*
+<nil>
+false
+*/
