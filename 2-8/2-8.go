dff5130eb5e6fef614e08c26dce88bdb5b0297db
Anthony <anthonyo@student.21-school.ru>
2025-10-20 22:55:38 +0300

    2-8

--- 2-8/2-8.go
+++ 2-8/2-8.go
@@ -1,5 +1,50 @@
 package main
 
-/*
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	"os"
+)
 
- */
+/*
+Получение точного времени (NTP)
+Создать программу, печатающую точное текущее время с использованием NTP-сервера.
+
+Реализовать проект как модуль Go.
+
+Использовать библиотеку ntp для получения времени.
+
+Программа должна выводить текущее время, полученное через NTP (Network Time Protocol).
+
+Необходимо обрабатывать ошибки библиотеки: в случае ошибки вывести её текст в STDERR и вернуть ненулевой код выхода.
+
+Код должен проходить проверки (vet и golint), т.е. быть написан идиоматически корректно.
+*/
+// Package main provides a command-line tool to fetch and display current time from NTP server.
+
+const defaultNTPServer = "pool.ntp.org"
+
+// getNTPTime fetches current time from the specified NTP server.
+func getNTPTime(server string) (string, error) {
+	time, err := ntp.Time(server)
+	if err != nil {
+		return "", fmt.Errorf("failed to get time from NTP server %q: %w", server, err)
+	}
+	return time.Format("2006-01-02 15:04:05.000 MST"), nil
+}
+
+func main() {
+	server := defaultNTPServer
+	if len(os.Args) > 1 {
+		server = os.Args[1]
+	}
+
+	timeStr, err := getNTPTime(server)
+	if err != nil {
+		log.SetOutput(os.Stderr)
+		log.Fatalf("Error: %v", err)
+	}
+
+	fmt.Println(timeStr)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/ZnNr/wb-level2
 
 go 1.25.0
+
+require github.com/beevik/ntp v1.5.0
+
+require (
+	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+)
